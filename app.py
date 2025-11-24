14d8f8cf69eef9d7255f87512f9273650b6e4d6e
Ahmed R <rabbi.ahmed@gmail.com>
2025-11-23 23:49:41 -0500

    Set debug and use_reloader False

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def hello_world():
         You can use it to automate the build, test, and deployment of a simple web application!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, use_reloader=False, host='0.0.0.0', port=5000)
